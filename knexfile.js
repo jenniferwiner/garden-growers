cdacd2bbd834579194b93a97ae4af9c0862b087d
Steven Hendricks <steven.hendricks@gmail.com>
2017-04-07 21:02:32 -0600

    heroku_deploy

--- knexfile.js
+++ knexfile.js
@@ -2,5 +2,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/gardengrowers_dev'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 }
