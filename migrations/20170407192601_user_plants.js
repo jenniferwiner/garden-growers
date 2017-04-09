267163dcb4a1944acca0e00906498977d2b35b0d
Derek Jones <derekjones@Dereks-MacBook-Pro.local>
2017-04-09 13:54:20 -0600

    description and timestamps added to user_plants migration Please enter the commit message for your changes. Lines starting

--- migrations/20170407192601_user_plants.js
+++ migrations/20170407192601_user_plants.js
@@ -4,6 +4,8 @@ exports.up = function(knex) {
       table.integer('user_id').notNullable().references('users.id').onDelete('cascade')
       table.integer('plant_id').notNullable().references('plants.id').onDelete('cascade')
       table.text('photo').notNullable().defaultTo('http://www.webweaver.nu/clipart/img/nature/flowers/potted-plant.png')
+      table.text('description')
+      table.timestamps(true, true)
     })
 };
 
