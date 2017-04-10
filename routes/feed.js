5c320332a9d49d2ef43685260ad542430543ebf5
jenniferwiner <jeniwiner@gmail.com>
2017-04-10 13:34:02 -0600

    mid /feed route, stopping to pull

--- routes/feed.js
+++ routes/feed.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
+var knex = require('../knex');
+var jwt = require('jsonwebtoken');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('feed');
+  knex('users')
+  .join('user_plants', 'users.id', '=', 'user_plants.user_id')
+  .join('plants', 'user_plants.plant_id', '=', 'plants.id')
+  .returning(['users.id', 'plants.garden_name', 'plants.scientific_name', 'user_plants.photo', 'users.garden_name', 'user_plants.description'])
+  .then((data) => {
+    console.log(data);
+    res.render('feed');
+  })
 });
 
 module.exports = router;
