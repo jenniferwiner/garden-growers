a9a2082c01c7f384537856873721a0dff36b6578
Jennifer Winer <jeniwiner@gmail.com>
2017-04-10 09:52:40 -0600

    First test working!

--- test/test-app.js
+++ test/test-app.js
@@ -3,20 +3,14 @@ process.env.NODE_ENV = 'test';
 const request = require('supertest');
 const expect = require('chai').expect;
 const app = require('../app');
+// const app = require('../bin/www')
 const knex = require('../knex');
-//
-// beforeEach(done => {
-//   Promise.all([
-//     knex('users').insert({ id: 1, gardenname: 'jerrysgarden', email: 'jerry@jerry.com', hashed_password: '$2a$12$C9AYYmcLVGYlGoO4vSZTPud9ArJwbGRsJ6TUsNULzR48z8fOnTXbS', is_admin: false }),
-//     knex('plants').insert({ id: 1, common_name: 'Ruellia', scientific_name: 'Ruellia' })
-//     ]).then(() => done());
-// });
 
-//
-// describe('GET /', () => {
-//   it('Load index page', done => {
-//     request(app)
-//       .get('/')
-//       .expect(200, done);
-//   });
-// });
+
+describe('GET /', () => {
+  it('Load index page', done => {
+    request(app)
+      .get('/')
+      .expect(200, done);
+  });
+});
