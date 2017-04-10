71d39a748ec3ad4d50ff3a7188ac15dc80fa01ca
jenniferwiner <jeniwiner@gmail.com>
2017-04-10 16:54:26 -0600

    Attempted insert plant

--- routes/home.js
+++ routes/home.js
@@ -9,84 +9,86 @@ let firstCharUpper = (data) => {
   let lowercase = data.toLowerCase();
   let firstLet = lowercase[0].toUpperCase();
   let editedPlant = firstLet + lowercase.slice(1);
+
   return editedPlant;
 }
 
-let insertPlant = (data) => knex('plants').insert(data).returning(['id', 'plants.common_name', 'plants.scientific_name'])
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    let userId
-    jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
-        if (payload) {
-            userId = payload.id
-            userPlants(userId)
-        }
-        else if (err){
-          res.redirect('/')
-        }
-    })
-
-    function userPlants(userId) {
-        knex('user_plants')
-            .where({
-                'user_id': userId
-            })
-            .innerJoin('users', 'users.id', 'user_plants.user_id')
-            .innerJoin('plants', 'plants.id', 'user_plants.plant_id')
-            .select(['users.garden_name', 'plants.common_name', 'plants.scientific_name', 'user_plants.description'])
-            .then((userPlants) => {
-                console.log('data', userPlants);
-                res.render('home', {
-                    userPlants
-                })
-            })
+  let user_id
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
+    if (payload) {
+      user_id = payload.id
+      userPlants(user_id)
     }
+    else if (err) {
+      res.redirect('/')
+    }
+  })
+
+  function userPlants(user_id) {
+    knex('user_plants')
+      .where({
+        'user_id': user_id
+      })
+      .innerJoin('users', 'users.id', 'user_plants.user_id')
+      .innerJoin('plants', 'plants.id', 'user_plants.plant_id')
+      .select(['users.garden_name', 'plants.common_name', 'plants.scientific_name', 'user_plants.description'])
+      .then((userPlants) => {
+        console.log('data', userPlants);
+        res.render('home', {
+          userPlants
+        })
+      })
+  }
 });
 
 router.post('/', function(req, res, next) {
-  knex('user_plants')
-    .innerJoin('users', 'users.id', 'user_plants.user_id')
-    .innerJoin('plants', 'plants.id', 'user_plants.plant_id')
-    .select(['users.garden_name', 'plants.common_name', 'plants.scientific_name', 'user_plants.description'])
-    .then((userPlants) => {
-      console.log('data', userPlants);
-      res.render('home', {
-        userPlants
-      })
-    })
-});
+  let scientific_name = firstCharUpper(req.body.scientific_name);
+  let common_name = firstCharUpper(req.body.common_name);
+  let plant_id;
+  let user_id;
+  let photo = req.body.photo;
+  let description = req.body.description;
+  let plant_count = req.body.plant_count;
 
-router.post('/home', function(req, res, next) {
-  let scientific_name = firstCharUpper(req.body.scientific_name)
-  let common_name = firstCharUpper(req.body.scientific_name)
+  function insertUserPlant() {
+    knex('user_plants')
+    .insert({ user_id, plant_id, photo, description, plant_count })
+    .then(() => {
+      res.redirect('/home')
+    })
+  }
+
+  // verify token
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
+    if (payload) {
+      user_id = payload.id
+      userPlants(user_id)
+    }
+    else if (err) {
+      res.redirect('/home')
+    }
+  });
 
   knex('plants')
     .where('common_name', common_name)
     .andWhere('scientific_name', scientific_name)
-    .then(data => {
-        if (data.length === 0) {
-
-          insertPlant({common_name, scientific_name}).then(data => {
-            console.log(data);
-            res.end()
-          })
-
-        }
-
-
-})
-
-
-  console.log(req.body);
-  insertPlant(req.body)
-    .then(data => {
-      console.log(data);
-      res.render('/home')
+    .then(searchedPlant => {
+      if (searchedPlant.length === 0) {
+        knex('plants')
+        .insert({ common_name, scientific_name }, '*')
+        .then(insertedPlant => {
+          plant_id = insertedPlant.id;
+          insertedPlant();
+        });
+      }
+      else {
+        plant_id = searchedPlant;
+        insertPlant();
+      }
     })
-    .catch((err) => {
-      next(err);
-    });
-})
+});
+
 
 module.exports = router;
