90e5c5342b952dc28fd62674b826f420a3580f84
Derek Jones <derekjones@Dereks-MacBook-Pro.local>
2017-04-10 12:57:46 -0600

    begin home get route

--- routes/home.js
+++ routes/home.js
@@ -1,13 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const knex = require('../knex')
+const boom = require('boom')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('home')
+  // knex('user_plants')
+  // .innerJoin('users', 'users.id','user_plants.user_id')
+  // .innerJoin('plants', 'plants.id', 'user_plants.plant_id')
+  // .returning('*')
+  // .then((data)=>{
+  //   console.log(data);
+  //   res.render('home')
+  // })
+res.render('home')
 });
 
 router.post('/', function (req, res, next) {
-  
 
 })
 
