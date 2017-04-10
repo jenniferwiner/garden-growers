7d7f0b595ea66866969492919483f79599704204
Derek Jones <derekjones@Dereks-MacBook-Pro.local>
2017-04-10 13:14:28 -0600

    fixed login

--- routes/home.js
+++ routes/home.js
@@ -5,14 +5,14 @@ const boom = require('boom')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  // knex('user_plants')
-  // .innerJoin('users', 'users.id','user_plants.user_id')
-  // .innerJoin('plants', 'plants.id', 'user_plants.plant_id')
-  // .returning('*')
-  // .then((data)=>{
-  //   console.log(data);
-  //   res.render('home')
-  // })
+  knex('user_plants')
+  .innerJoin('users', 'users.id','user_plants.user_id')
+  .innerJoin('plants', 'plants.id', 'user_plants.plant_id')
+  .returning('*')
+  .then((data)=>{
+    console.log(data);
+    res.render('home')
+  })
 res.render('home')
 });
 
