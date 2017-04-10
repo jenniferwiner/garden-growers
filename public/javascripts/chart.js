a6869b0e144b8e4fbf5dca9fd82f3f2096c077b0
TomGarrison <Tomgarrison2011@gmail.com>
2017-04-10 10:40:44 -0600

    frontend magic

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -1,54 +1,57 @@
 $(document).ready(() => {
-  var ctd = $('#doughnutChart')
+  // DEMO CHARTS START
+  var ctd = $('#doughnutChartDemo')
   var myDoughnutChart = new Chart(ctd, {
       type: 'doughnut',
       data: {
         labels: [
-            "Red",
-            "Blue",
-            "Yellow"
+            "Evergreen",
+            "Fruit",
+            "Veggies",
         ],
         datasets: [
             {
-                data: [300, 50, 100],
+                data: [1, 1, 15],
                 backgroundColor: [
-                    "#FF6384",
-                    "#36A2EB",
-                    "#FFCE56"
+                    "#7E6551",
+                    "#A4AF69",
+                    "#FBFFB9",
+                    "#666534"
                 ],
                 hoverBackgroundColor: [
-                    "#FF6384",
-                    "#36A2EB",
-                    "#FFCE56"
+                    "#7E6551",
+                    "#A4AF69",
+                    "#FBFFB9",
+                    "#666534"
                 ]
             }]
     }
   });
 
-var ctx = $("#myChart")
+var ctx = $("#myChartDemo")
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["Norfolk Island Pine", "Grape Ivy", "Basil", "Green Beans", "Spanich", "Orange Tree"],
         datasets: [{
             label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            data: [1, 2, 3, 5, 2, 3],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
+               "rgba(126, 101, 81, 0.5)",
+               "rgba(164, 175, 105, 0.5)",
+               "rgba(251, 255, 185, 0.5)",
+               "rgba(224, 205, 121, 0.5)",
+               "rgba(224, 222, 114, 0.5)",
+               "rgba(75, 80, 48, 0.5)"
+             ],
+             borderColor: [
+               "#7E6551",
+               "#A4AF69",
+               "#FBFFB9",
+               '#E1CE7A',
+               '#E0DE72',
+               '#4B5030'
+             ],
             borderWidth: 1
         }]
     },
@@ -61,5 +64,73 @@ var myChart = new Chart(ctx, {
             }]
         }
     }
+});
+// DEMO CHARTS END
+
+// /HOME CHARTS START
+var ctd = $('#doughnutChartHome')
+var myDoughnutChart = new Chart(ctd, {
+    type: 'doughnut',
+    data: {
+      labels: [
+          "Evergreen",
+          "Fruit",
+          "Veggies",
+      ],
+      datasets: [
+          {
+              data: [1, 1, 15],
+              backgroundColor: [
+                  "#7E6551",
+                  "#A4AF69",
+                  "#FBFFB9",
+                  "#666534"
+              ],
+              hoverBackgroundColor: [
+                  "#7E6551",
+                  "#A4AF69",
+                  "#FBFFB9",
+                  "#666534"
+              ]
+          }]
+  }
+});
+
+var ctx = $("#myChartHome")
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+      labels: ["Norfolk Island Pine", "Grape Ivy", "Basil", "Green Beans", "Spanich", "Orange Tree"],
+      datasets: [{
+          label: '# of Votes',
+          data: [1, 2, 3, 5, 2, 3],
+          backgroundColor: [
+             "rgba(126, 101, 81, 0.5)",
+             "rgba(164, 175, 105, 0.5)",
+             "rgba(251, 255, 185, 0.5)",
+             "rgba(224, 205, 121, 0.5)",
+             "rgba(224, 222, 114, 0.5)",
+             "rgba(75, 80, 48, 0.5)"
+           ],
+           borderColor: [
+             "#7E6551",
+             "#A4AF69",
+             "#FBFFB9",
+             '#E1CE7A',
+             '#E0DE72',
+             '#4B5030'
+           ],
+          borderWidth: 1
+      }]
+  },
+  options: {
+      scales: {
+          yAxes: [{
+              ticks: {
+                  beginAtZero:true
+              }
+          }]
+      }
+  }
 });
 })
