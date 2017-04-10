6800d6e588fad95862508d1f38401eb37abbaeab
jenniferwiner <jeniwiner@gmail.com>
2017-04-10 13:47:29 -0600

    ALL users' plants displaying on feed- WORKING.

--- routes/feed.js
+++ routes/feed.js
@@ -7,10 +7,9 @@ router.get('/', function(req, res, next) {
   knex('users')
   .join('user_plants', 'users.id', '=', 'user_plants.user_id')
   .join('plants', 'user_plants.plant_id', '=', 'plants.id')
-  .returning(['users.id', 'plants.garden_name', 'plants.scientific_name', 'user_plants.photo', 'users.garden_name', 'user_plants.description'])
-  .then((data) => {
-    console.log(data);
-    res.render('feed');
+  .select(['users.id', 'plants.common_name', 'plants.scientific_name', 'user_plants.photo', 'users.garden_name', 'user_plants.description', 'user_plants.created_at'])
+  .then((feedData) => {
+    res.render('feed', { feedData });
   })
 });
 
