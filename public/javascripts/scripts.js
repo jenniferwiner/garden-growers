46e142fb4e68fde382882da7bb953aca55be80be
Steven Hendricks <steven.hendricks@gmail.com>
2017-04-11 11:42:04 -0600

    weather_API

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,52 +1,170 @@
 $(document).ready(() => {
-    // delete ajax call
-    $('.delete').click(() => {
+      // delete ajax call
+      $('.delete').click(() => {
         let id = $(event.target).attr('data-id')
         $.ajax({
-            method: 'DELETE',
-            url: '/home',
-            data: {
-                id: id
-            },
-            success: (data) => {
-                if (data) {
-                    location.reload()
-                }
-            },
-            error: () => {}
+          method: 'DELETE',
+          url: '/home',
+          data: {
+            id: id
+          },
+          success: (data) => {
+            if (data) {
+              location.reload()
+            }
+          },
+          error: () => {}
         })
-    })
-    // toggle edit plant
-    $('.editBtn').click(() => {
-      $('.editForm').toggleClass('hidden')
+      })
+      // toggle edit plant
+      $('.editBtn').click(() => {
+            $('.editForm').toggleClass('hidden')
 
-    $('.editSubmit').click(() => {
-        let id = $(event.target).attr('data-id')
-        let common_name =
-        let
-        $.ajax({
-            method: 'PATCH',
-            url: '/home',
-            data: {
-                id: id
-            },
-            success: (data) => {
-              if (data){
-                location.reload()
-              }
-            },
-            error: () => {}
+            $('.editSubmit').click(() => {
+              let id = $(event.target).attr('data-id')
+              let common_name =
+                let
+                  $.ajax({
+                    method: 'PATCH',
+                    url: '/home',
+                    data: {
+                      id: id
+                    },
+                    success: (data) => {
+                      if (data) {
+                        location.reload()
+                      }
+                    },
+                    error: () => {}
+                  })
+            })
+            // toggle to add new plant
+            // toggle to add new plant
+            $('#createPlantBtnDemo').click(() => {
+              $('.createForm').toggleClass('hidden')
+            })
+            $('#createPlantBtn').click(() => {
+              $('.createForm').toggleClass('hidden')
+            })
+            // random qutoe gen
+            let quoteArray = ["Flowers always make people better, happier, and more helpful; they are sunshine, food and medicine for the soul. -Luther Burbank", "In the spring, at the end of the day, you should smell like dirt. -Margaret Atwood", "If you have a garden and a library, you have everything you need.  -Marcus Tullius Cicero", "Flowers are restful to look at. They have neither emotions nor conflicts. -Sigmund Freud", "To see a world in a grain of sand and heaven in a wild flower Hold infinity in the palms of your hand and eternity in an hour.  -William Blake", "A garden must combine the poetic and the mysterious with a feeling of serenity and joy.  -Luis Barragan", "Show me your garden and I shall tell you what you are.  -Alfred Austin", "Weeds are flowers too, once you get to know them.  -A. A. Milne", "What is a weed? A plant whose virtues have never been discovered.  -Ralph Waldo Emerson"]
+            var randomQuote = quoteArray[Math.floor(Math.random() * quoteArray.length)]
+            $('.randomQuote').append(randomQuote)
+
+            console.log("userZipCode=", userZipCode);
+
+            // ///////AJAX///// //
+            $.ajax({
+              method: 'GET',
+
+              // WU KEY      155ad056960470aa
+              url: `http://api.wunderground.com/api/155ad056960470aa/conditions/q/${userZipCode}.json`,
+
+              datatype: 'json',
+
+              success: function(data) {
+                let weatherInfo = data
+                console.log(weatherInfo);
+
+                // Filling the weather object
+                let weatherObj = {
+                  humidity: {},
+                  temp: {},
+                  windSp: {},
+                  windStr: {},
+                  windGust: {},
+                  windDir: {},
+                  precip: {},
+                  icon: {},
+                  vis: {},
+                  forecast: {},
+                  pressure: {}
+                }
+
+                let obs = weatherInfo.current_observation
+
+                // METRIC
+                weatherObj.humidity.eu = obs.relative_humidity
+                weatherObj.temp.eu = obs.temp_c
+                weatherObj.windSp.eu = obs.wind_kph
+                weatherObj.windStr.eu = obs.wind_string
+                weatherObj.windGust.eu = obs.wind_gust_kph
+                weatherObj.windDir.eu = obs.wind_dir
+                weatherObj.precip.eu = obs.precip_today_metric
+                weatherObj.icon.eu = obs.icon_url
+                weatherObj.vis.eu = obs.visibility_km
+                weatherObj.forecast.eu = obs.weather
+                weatherObj.pressure.eu = obs.pressure_mb
+
+                // US
+                weatherObj.humidity.us = obs.relative_humidity
+                weatherObj.temp.us = obs.temp_f
+                weatherObj.windSp.us = obs.wind_mph
+                weatherObj.windStr.us = obs.wind_string
+                weatherObj.windGust.us = obs.wind_gust_mph
+                weatherObj.windDir.us = obs.wind_dir
+                weatherObj.precip.us = obs.precip_today_in
+                weatherObj.icon.us = obs.icon_url
+                weatherObj.vis.us = obs.visibility_mi
+                weatherObj.forecast.us = obs.weather
+                weatherObj.pressure.us = obs.pressure_in
+
+
+                function us() {
+                  $('#header-icon').append('Conditions <img src=' + weatherObj.icon.us + '>')
+                  $('#forecast').append('<h4>' + weatherObj.forecast.us + '</h4>')
+                  $('#pressure').append('<h4>' + weatherObj.pressure.us + ' in' + '</h4>')
+                  $('#humidity').append('<h4>' + weatherObj.humidity.us + '</h4>')
+                  $('#temp').append('<h4>' + weatherObj.temp.us + ' \u00B0f' + '</h4>')
+                  $('#precip').append('<h4>' + weatherObj.precip.us + ' in' + '</h4>')
+                  $('#wSpeed').append('<h4>' + weatherObj.windSp.us + ' mph' + '</h4>')
+                  $('#vis').append('<h4>' + weatherObj.vis.us + ' mi' + '</h4>')
+                  $('#wind-condition').append('<h4>' + weatherObj.windStr.us + '</h4>')
+                  $('#bearing').append('<h4>' + weatherObj.windDir.us + '</h4>')
+                  $('#gust').append('<h4>' + weatherObj.windGust.us + ' mph' + '</h4>')
+                }
+                us()
+
+                function eu() {
+                  $('#header-icon').append('Conditions <img src=' + weatherObj.icon.eu + '>')
+                  $('#forecast').append('<h4>' + weatherObj.forecast.eu + '</h4>')
+                  $('#pressure').append('<h4>' + weatherObj.pressure.eu + ' mb' + '</h4>')
+                  $('#humidity').append('<h4>' + weatherObj.humidity.eu + '</h4>')
+                  $('#temp').append('<h4>' + weatherObj.temp.eu + ' \u00B0c' + '</h4>')
+                  $('#precip').append('<h4>' + weatherObj.precip.eu + ' mm' + '</h4>')
+                  $('#wSpeed').append('<h4>' + weatherObj.windSp.eu + ' kph' + '</h4>')
+                  $('#vis').append('<h4>' + weatherObj.vis.eu + ' km' + '</h4>')
+                  $('#wind-condition').append('<h4>' + weatherObj.windStr.eu + '</h4>')
+                  $('#bearing').append('<h4>' + weatherObj.windDir.eu + '</h4>')
+                  $('#gust').append('<h4>' + weatherObj.windGust.eu + ' kph' + '</h4>')
+                }
+
+                let toggle = true;
+
+                // METRIC TOGGLE
+                $('#toggle').click(function() {
+                  toggle = !toggle
+
+                  $('#header-icon').text('')
+                  $('#forecast').text('')
+                  $('#pressure').text('')
+                  $('#humidity').text('')
+                  $('#temp').text('')
+                  $('#precip').text('')
+                  $('#wSpeed').text('')
+                  $('#vis').text('')
+                  $('#wind-condition').text('')
+                  $('#bearing').text('')
+                  $('#gust').text('')
+
+                  if (toggle) {
+                    us()
+                  } else {
+                    eu()
+                  }
+                })
+              },
+              error: function() {}
+            })
+          })
         })
-    })
-    // toggle to add new plant
-    $('#createPlantBtnDemo').click(() => {
-        $('.createForm').toggleClass('hidden')
-    })
-    $('#createPlantBtn').click(() => {
-        $('.createForm').toggleClass('hidden')
-    })
-    // random qutoe gen
-    let quoteArray = ["Flowers always make people better, happier, and more helpful; they are sunshine, food and medicine for the soul. -Luther Burbank", "In the spring, at the end of the day, you should smell like dirt. -Margaret Atwood", "If you have a garden and a library, you have everything you need.  -Marcus Tullius Cicero", "Flowers are restful to look at. They have neither emotions nor conflicts. -Sigmund Freud", "To see a world in a grain of sand and heaven in a wild flower Hold infinity in the palms of your hand and eternity in an hour.  -William Blake", "A garden must combine the poetic and the mysterious with a feeling of serenity and joy.  -Luis Barragan", "Show me your garden and I shall tell you what you are.  -Alfred Austin", "Weeds are flowers too, once you get to know them.  -A. A. Milne", "What is a weed? A plant whose virtues have never been discovered.  -Ralph Waldo Emerson"]
-    var randomQuote = quoteArray[Math.floor(Math.random() * quoteArray.length)]
-    $('.randomQuote').append(randomQuote)
-})
--- routes/home.js
+++ routes/home.js
@@ -6,211 +6,218 @@ const jwt = require('jsonwebtoken')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    let user_id;
-    jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
-        if (payload) {
-            user_id = payload.id;
-            userPlants(user_id);
-        } else if (err) {
-            res.redirect('/');
-        }
-    });
-
-    function userPlants(user_id) {
-        knex('user_plants')
-            .where('user_id', user_id)
-            .then((data) => {
-                if (data.length === 0) {
-                    knex('users')
-                        .where('id', user_id)
-                        .then(userData => {
-                            res.render('home', {
-                                gardenName: userData[0].garden_name,
-                                numberOfPlants: 0
-                            })
-                        })
-                } else {
-                    Promise.all([findUserPlants(user_id), countUserPlants(user_id), commonPlants(user_id)])
-                        .then(values => {
-                            let [userPlants, count, commonPlants] = values;
-                            res.render('home', {
-                                gardenName: userPlants[0].garden_name,
-                                userPlants,
-                                numberOfPlants: count[0].count,
-                                commonPlants
-                            });
-                        })
-                }
-
-            });
+  let user_id;
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
+    if (payload) {
+      user_id = payload.id;
+      userPlants(user_id);
+    } else if (err) {
+      res.redirect('/');
     }
+
+    // master...
+    function userPlants(user_id) {
+      knex('user_plants')
+        .where('user_id', user_id)
+        .then((data) => {
+          if (data.length === 0) {
+            knex('users')
+              .where('id', user_id)
+              .then(userData => {
+                res.render('home', {
+                  gardenName: userData[0].garden_name,
+                  numberOfPlants: 0
+                })
+              })
+          } else {
+            Promise.all([findUserPlants(user_id), countUserPlants(user_id), commonPlants(user_id)])
+              .then(values => {
+                let [userPlants, count, commonPlants] = values;
+                res.render('home', {
+                  gardenName: userPlants[0].garden_name,
+                  userPlants,
+                  numberOfPlants: count[0].count,
+                  commonPlants,
+                  zip: userPlants[0].zipcode
+                });
+              })
+          }
+
+        });
+    }
+
+  });
 });
 
 router.get('/labelsAndData', function(req, res, next) {
-    function userPlantforGraph(user_id) {
-        knex('user_plants')
-            .join('plants', 'user_plants.plant_id', '=', 'plants.id')
-            .where('user_plants.user_id', user_id)
-            .select('plants.common_name', 'plant_count')
-            .then((data) => {
-                let chartData = data.map(plant => {
-                    return plant.plant_count;
-                });
-                let chartLabels = data.map(plant => {
-                    return plant.common_name;
-                });
-                res.set('Content-Type', 'application/json');
-                res.send({
-                    data: chartData,
-                    labels: chartLabels
-                });
-            })
-            .catch((err) => {
-                res.status(404);
-                res.send("Couldn't find UserId");
-            });
+  function userPlantforGraph(user_id) {
+    knex('user_plants')
+      .join('plants', 'user_plants.plant_id', '=', 'plants.id')
+      .where('user_plants.user_id', user_id)
+      .select('plants.common_name', 'plant_count')
+      .then((data) => {
+        let chartData = data.map(plant => {
+          return plant.plant_count;
+        });
+        let chartLabels = data.map(plant => {
+          return plant.common_name;
+        });
+        res.set('Content-Type', 'application/json');
+        res.send({
+          data: chartData,
+          labels: chartLabels
+        });
+      })
+      .catch((err) => {
+        res.status(404);
+        res.send("Couldn't find UserId");
+      });
+  }
+
+  let user_id;
+
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
+    if (payload) {
+      user_id = payload.id;
+      userPlantforGraph(user_id);
+    } else if (err) {
+      res.redirect('/');
     }
-
-    let user_id;
-
-    jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
-        if (payload) {
-            user_id = payload.id;
-            userPlantforGraph(user_id);
-        } else if (err) {
-            res.redirect('/');
-        }
-    });
+  });
 });
 
 router.post('/', function(req, res, next) {
-    // setting variables for knex insert
-    let scientific_name = firstCharUpper(req.body.scientific_name);
-    let common_name = firstCharUpper(req.body.common_name);
-    let plant_id;
-    let user_id;
-    let photo = req.body.photo;
-    let description = req.body.description;
-    let plant_count = req.body.plant_count;
+  // setting variables for knex insert
+  let scientific_name = firstCharUpper(req.body.scientific_name);
+  let common_name = firstCharUpper(req.body.common_name);
+  let plant_id;
+  let user_id;
+  let photo = req.body.photo;
+  let description = req.body.description;
+  let plant_count = req.body.plant_count;
 
-    // verify token
-    jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
-        if (payload) {
-            user_id = payload.id;
-        } else if (err) {
-            res.redirect('/home');
-        }
-    });
+  // verify token
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
+    if (payload) {
+      user_id = payload.id;
+    } else if (err) {
+      res.redirect('/home');
+    }
+  });
 
-    knex('plants')
-        .where('common_name', common_name)
-        .andWhere('scientific_name', scientific_name)
-        .then(searchedPlant => {
-            if (searchedPlant.length === 0) {
-                knex('plants')
-                    .insert({
-                        common_name,
-                        scientific_name
-                    }, '*')
-                    .then(insertedPlant => {
-                        plant_id = insertedPlant[0].id;
-                        insertUserPlant();
-                    });
-            } else {
-                plant_id = searchedPlant[0].id;
-                insertUserPlant();
-            }
-        })
+  knex('plants')
+    .where('common_name', common_name)
+    .andWhere('scientific_name', scientific_name)
+    .then(searchedPlant => {
+      if (searchedPlant.length === 0) {
+        knex('plants')
+          .insert({
+            common_name,
+            scientific_name
+          }, '*')
+          .then(insertedPlant => {
+            plant_id = insertedPlant[0].id;
+            insertUserPlant();
+          });
+      } else {
+        plant_id = searchedPlant[0].id;
+        insertUserPlant();
+      }
+    })
 });
 
 function firstCharUpper(data) {
-    let lowercase = data.toLowerCase();
-    let firstLet = lowercase[0].toUpperCase();
-    let editedPlant = firstLet + lowercase.slice(1);
-    return editedPlant;
+  let lowercase = data.toLowerCase();
+  let firstLet = lowercase[0].toUpperCase();
+  let editedPlant = firstLet + lowercase.slice(1);
+  return editedPlant;
 };
 
 function countUserPlants(user_id) {
-    return knex('user_plants').where('user_id', user_id).count('*');
+  return knex('user_plants').where('user_id', user_id).count('*');
 }
 
 function findUserPlants(user_id) {
-    return knex('user_plants')
-        .where('user_id', user_id)
-        .join('users', 'users.id', 'user_plants.user_id')
-        .join('plants', 'plants.id', 'user_plants.plant_id')
-        .select(['users.garden_name', 'plants.common_name', 'plants.scientific_name', 'user_plants.description', 'user_plants.photo', 'user_plants.created_at', 'user_plants.plant_id'])
+  return knex('user_plants')
+    .where('user_id', user_id)
+    .join('users', 'users.id', 'user_plants.user_id')
+    .join('plants', 'plants.id', 'user_plants.plant_id')
+    .select(['users.garden_name', 'plants.common_name', 'plants.scientific_name', 'user_plants.description', 'user_plants.photo', 'user_plants.created_at', 'user_plants.plant_id', 'users.zipcode'])
 }
 
 function commonPlants(user_id) {
-    return knex('user_plants')
-        .join('plants', 'plants.id', '=', 'user_plants.plant_id')
-        .where('user_id', user_id)
-        .select('common_name', 'plant_count')
-        .orderBy('plant_count', 'desc')
-        .limit(3)
+  return knex('user_plants')
+    .join('plants', 'plants.id', '=', 'user_plants.plant_id')
+    .where('user_id', user_id)
+    .select('common_name', 'plant_count')
+    .orderBy('plant_count', 'desc')
+    .limit(3)
 }
 
 function insertUserPlant() {
-    knex('user_plants')
-        .insert({
-            user_id,
-            plant_id,
-            photo,
-            description,
-            plant_count
-        })
-        .then(() => {
-            res.redirect('/home');
-        })
+  knex('user_plants')
+    .insert({
+      user_id,
+      plant_id,
+      photo,
+      description,
+      plant_count
+    })
+    .then(() => {
+      res.redirect('/home');
+    })
 }
 router.patch('/', function(req, res, next) {
-    let scientific_name = firstCharUpper(req.body.scientific_name);
-    let common_name = firstCharUpper(req.body.common_name);
-    let plant_id = req.body.plant_id
-    let user_id;
-    let photo = req.body.photo;
-    let description = req.body.description;
-    let plant_count = req.body.plant_count;
-    let newPlantData = req.body;
-    // verify token
-    jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
-        if (payload) {
-            user_id = payload.id;
-        } else if (err) {
-            res.redirect('/home');
-        }
-    });
-    knex('user_plants')
-        .where({
-            user_id,
-            plant_id
-        })
-        .returning(['common_name', 'scientific_name', 'photo', 'description', 'plant_count'])
-        .update(newPlantData)
-        .then((data) => {
-            console.log(data);
-            res.send(data[0])
-        })
+  let scientific_name = firstCharUpper(req.body.scientific_name);
+  let common_name = firstCharUpper(req.body.common_name);
+  let plant_id = req.body.plant_id
+  let user_id;
+  let photo = req.body.photo;
+  let description = req.body.description;
+  let plant_count = req.body.plant_count;
+  let newPlantData = req.body;
+  // verify token
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
+    if (payload) {
+      user_id = payload.id;
+    } else if (err) {
+      res.redirect('/home');
+    }
+  });
+  knex('user_plants')
+    .where({
+      user_id,
+      plant_id
+    })
+    .returning(['common_name', 'scientific_name', 'photo', 'description', 'plant_count'])
+    .update(newPlantData)
+    .then((data) => {
+      console.log(data);
+      res.send(data[0])
+    })
 
 })
-}
 
 // del ajax
 router.delete('/', (req, res, next) => {
-    let id = req.body.id
-    let userId = 0;
-    jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
-        if (payload) {
-            userId = payload.id;
-            knex('user_plants').del().where({
-                user_id: userId,
-                plant_id: id
-            }).debug(true).then(() => {
-                res.send(200)
-            })
-        }
-    })
+  let id = req.body.id
+  let userId = 0;
+  jwt.verify(req.cookies.token, process.env.JWT_KEY, (err, payload) => {
+    if (payload) {
+      userId = payload.id;
+      knex('user_plants').del().where({
+        user_id: userId,
+        plant_id: id
+      }).debug(true).then(() => {
+        res.send(200)
+      })
+    }
+  })
 })
+
+
+
+
+
 
 module.exports = router;
