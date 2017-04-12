db7d94eb5b1378e90a6992c8120d480943e5d194
Derek Jones <djamesjones4@gmail.com>
2017-04-12 09:50:31 -0600

    home post validation next patch val

--- validations/home.js
+++ validations/home.js
@@ -4,22 +4,24 @@ const Joi = require('joi')
 
 module.exports.post = {
   body: {
-    email: Joi.string()
-      .label('Email')
+    common_name: Joi.string()
+      .label('Common Name')
       .required()
       .email()
       .trim(),
-    password: Joi.string()
-      .label('Password')
+    scientific_name: Joi.string()
+      .label('Scientific Name')
       .required()
       .trim(),
-    garden_name: Joi.string()
-      .label('Garden Name')
-      .required()
-      .trim(),
-    zipcode: Joi.number()
+    plant_count: Joi.number()
       .integer()
+      .label('Number of Plant Types')
       .required()
+      .trim()
   }
 }
-module.exports.patch = {}
+module.exports.patch = {
+  body: {
+    
+  }
+}
