fd35d353202b46a0d13e3ade9ef5dedfebcbd8d8
Derek Jones <djamesjones4@gmail.com>
2017-04-12 00:28:46 -0600

    beginning server validation for home

---
+++ validations/home.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const Joi = require('joi')
+
+module.exports.post = {
+  body: {
+    email: Joi.string()
+      .label('Email')
+      .required()
+      .email()
+      .trim(),
+    password: Joi.string()
+      .label('Password')
+      .required()
+      .trim(),
+    garden_name: Joi.string()
+      .label('Garden Name')
+      .required()
+      .trim(),
+    zipcode: Joi.number()
+      .integer()
+      .required()
+  }
+}
+module.exports.patch = {}
