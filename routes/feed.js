16c7d5d3d94354f4d0e880701c0268e311219acf
jenniferwiner <jeniwiner@gmail.com>
2017-04-11 21:26:26 -0600

    feed/:id renters count and type of plants, chart still unfinished

--- routes/feed.js
+++ routes/feed.js
@@ -15,15 +15,38 @@ router.get('/', (req, res, next) => {
 
 router.get('/:id', (req, res, next) => {
   let userId = req.params.id
-  knex('users')
-  .join('user_plants', 'users.id', '=', 'user_plants.user_id')
-  .join('plants', 'user_plants.plant_id', '=', 'plants.id')
-  .select(['users.id', 'plants.common_name', 'plants.scientific_name', 'user_plants.photo', 'users.garden_name', 'user_plants.description', 'user_plants.created_at'])
-  .where('users.id', userId)
-  .then((gardenData) => {
-    let gardenName = gardenData[0].garden_name
-    res.render('user', { gardenData, gardenName })
-  })
+
+  Promise.all([findUserPlants(userId), countUserPlants(userId), commonPlants(userId)])
+    .then(values => {
+      let [userPlantsArr, count, commonPlantsArr] = values
+      res.render('user', {
+        gardenName: userPlantsArr[0].garden_name,
+        gardenData: userPlantsArr,
+        numberOfPlants: count[0].count,
+        commonPlants: commonPlantsArr
+      })
+    })
 })
+
+function countUserPlants(user_id) {
+  return knex('user_plants').where('user_id', user_id).count('*')
+}
+
+function findUserPlants(user_id) {
+  return knex('user_plants')
+    .where('user_id', user_id)
+    .join('users', 'users.id', 'user_plants.user_id')
+    .join('plants', 'plants.id', 'user_plants.plant_id')
+    .select(['users.garden_name', 'plants.common_name', 'plants.scientific_name', 'user_plants.description', 'user_plants.photo', 'user_plants.updated_at', 'user_plants.plant_id', 'users.zipcode', 'user_plants.plant_count', 'user_plants.id'])
+}
+
+function commonPlants(user_id) {
+  return knex('user_plants')
+    .join('plants', 'plants.id', '=', 'user_plants.plant_id')
+    .where('user_id', user_id)
+    .select('common_name', 'plant_count')
+    .orderBy('plant_count', 'desc')
+    .limit(3)
+}
 
 module.exports = router
